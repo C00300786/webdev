8df49911a1ae93c7676916568ea2d26af488235a
c00300786 <c00300786@setu.ie>
2025-01-16 15:56:43 +0000

    task 6

--- lab1/task5.php
+++ lab1/task5.php
@@ -10,17 +10,17 @@
     <?php
         
         echo date("M/d/Y");
-        echo <br>;
-        
+        echo "<br>";
+
         echo date("D/M/Y");
-        echo <br>;
-        
+        echo "<br>";
+
         echo date("d/m/Y");
-        echo <br>;
-        
+        echo "<br>";
+
         echo date("d/n/y");
-        echo <br>;
-        
+        echo "<br>";
+
         echo date("l,m,d,Y");
     ?>
     
