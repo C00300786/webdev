425fdd5a69e9031e7565b715761ed58adaeb3301
c00300786 <c00300786@setu.ie>
2025-01-16 15:42:04 +0000

    task5

---
+++ lab1/task5.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        echo date();
+        echo "";
+        echo date("M/d/Y");
+        echo date();
+        echo "";
+        echo date("l/M/Y");
+        echo date()
+        echo ""
+        echo date("D/M/Y")
+        echo date()
+        echo ""
+        echo date("l,m,d,Y")
+    ?>
+    
+</body>
+</html>
