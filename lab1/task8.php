230541aac91456b7cf7170225644ff343f16fa9c
C00300786 <C0300786@setu.ie>
2025-01-23 11:03:53 +0000

    task 8

--- lab1/task8.php
+++ lab1/task8.php
@@ -6,12 +6,37 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Find the Day of the Week</h1>
+    <form method="post" action="">
+        <label for="day">Day:</label>
+        <input type="number" id="day" name="day" min="1" max="31" required>
+        <br><br>
+        <label for="month">Month:</label>
+        <input type="number" id="month" name="month" min="1" max="12" required>
+        <br><br>
+        <label for="year">Year:</label>
+        <input type="number" id="year" name="year" min="1" required>
+        <br><br>
+        <button type="submit">Find Day</button>
+    </form>
 
     <?php
-        
-      $cart = array("Cereal" => 5.00,"Coffee" => 2.50,"Bannan" => 3.50, "Onions" => 1.00,"Lettuce" => 2.40, "tomatos" => 3.50) ;
-      var_dump( $cart );
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $day = $_POST['day'];
+        $month = $_POST['month'];
+        $year = $_POST['year'];
+
+        // Validate date
+        if (checkdate($month, $day, $year)) {
+            // Get the day of the week
+            $date = "$year-$month-$day";
+            $dayOfWeek = date('l', strtotime($date));
+            echo "<h2>The day of the week for $date is $dayOfWeek.</h2>";
+        } else {
+            echo "<h2>Invalid date. Please enter a valid date.</h2>";
+        }
+    }
     ?>
-    
+
 </body>
 </html>
