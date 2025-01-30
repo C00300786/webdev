a64155a857cc4a271a33f91ad0874d6cbdcd3b0a
c00300786 <c00300786@setu.ie>
2025-01-30 11:45:48 +0000

    yuh

--- lab2/task1.php
+++ lab2/task1.php
@@ -5,9 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<body>
+    <form method="update" action="">
+            <label for="Fname">:enter your first name</label>
+            <input id="Fname">
+            <br>
+            <label for="lname">:enter your last name</label>
+            <input id="lname">
+            <br>
+            <button type="submit">Enter</button>
+    </form>
     <?php
-        
-            echo"<h2>have a good day $_POST[Fname] $_POST[lname]"
+        if ($_SERVER['REQUEST_METHOD'] === 'UPDATE')
+        {
+            echo"<h2>have a good day $Fname $lname"
+        }
     ?>
 </body>
 </html>
