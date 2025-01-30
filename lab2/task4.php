8700e6c346546f0ddffbaa509edcaadb7f38010a
c00300786 <c00300786@setu.ie>
2025-01-30 12:59:15 +0000

    yut

---
+++ lab2/task4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="showDetails.php">
+        <p>
+            <label for="feet">Feet</label>
+            <input type="text" name="feet" id="feet" placeholder="Feet" required>
+        </p>
+        <p>
+            <label for="inches"></label>
+            <input type="text" name="inches" id="inches"placeholder="Inches"required>
+        </p>
+        <input type="Submit" value="Submit">
+        <input type="Reset" value="Clear">
+    </form>
+</body>
+</html>
