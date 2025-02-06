549a5b0c7b0e8904eb09b9793eb1d40fde9879e1
c00300786 <c00300786@setu.ie>
2025-02-06 13:02:46 +0000

    task2 upd4

--- lab3/task2/displayview2.php
+++ lab3/task2/displayview2.php
@@ -14,21 +14,21 @@ if ($rowcount == 1)
 {
     $row = mysqli_fetch_array($result);
 
-    $_SESSION('personid')=$_row('personid');
-    $_SESSION('firstname')=$_row('firstname');
-    $_SESSION('lastname')=$_row('lastname');
-    $_SESSION('dob')=$_row('DOB');
-    $_SESSION('email')=$_row('Email');
-    $_SESSION('phone')=$_row('Phone');
+    $_SESSION['personid']=$_row['personid'];
+    $_SESSION['firstname']=$_row('firstname');
+    $_SESSION['lastname']=$_row('lastname');
+    $_SESSION['dob']=$_row('DOB');
+    $_SESSION['email']=$_row('Email');
+    $_SESSION['phone']=$_row('Phone');
 }else if($rowcount == 0)
 
 {
-    unset $_SESSION('personid');
-    unset $_SESSION('firstname');
-    unset $_SESSION('lastname');
-    unset $_SESSION('dob');
-    unset $_SESSION('email');
-    unset $_SESSION('phone'); 
+    unset ($_SESSION['personid']);
+    unset ($_SESSION['firstname']);
+    unset ($_SESSION['lastname']);
+    unset ($_SESSION['dob']);
+    unset ($_SESSION['email']);
+    unset ($_SESSION['phone']); 
 }
 mysqli_close($con)
 
