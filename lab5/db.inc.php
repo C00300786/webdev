d445b6826e3ffb6a6bf66f1d10e3744c800f6060
c00300786 <c00300786@setu.ie>
2025-03-13 10:25:27 +0000

    1

--- lab5/db.inc.php
+++ lab5/db.inc.php
@@ -1,19 +1,13 @@
-<!--
-    Name: Jamie WIlliamson
-    ID: C00300786
--->
 <?php
-// login info for database
-$hostname = "localhost";
-$username = "Timo";
-$password = "labuser3606";
+    $hostname = "localhost";
+    $username = "C00300786";
+    $password = "Wordpass@123@";
+    $dbname = "MYDBc00300786";
 
-$dbname = "MyDBC00295555";
-// connecting to database
-$con = mysqli_connect($hostname, $username, $password, $dbname);
-// if it doesnt connect run the error message
-if (!$con)
-{
-    die("Failed to connect to MYSQL: " . mysqli_connect_error());
-}
-?>
+    $con = mysqli_connect($hostname,$username,$password,$dbname);
+
+    if (!$con)
+    {
+        die("Failed to connect". mysqli_connect_error());
+    }
+    ?>
