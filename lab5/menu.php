7d8c647ad73f6cb90fa6e093b8cddc8f6ff5ab72
c00300786 <c00300786@setu.ie>
2025-03-20 09:59:30 +0000

    t5

---
+++ lab5/menu.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+
+<style>
+a:link, a:visited { /*Creates a button-like link*/
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active { /* Hovering over button link changes its colour */
+  background-color: red;
+}
+</style>
+</head>
+
+<body>
+<h1>Menu System</h1>
+
+<a href="menu.php">Home</a>
+<a href="insert.html.php">Add a Record</a>
+<a href="view.html.php">View a Record</a>
+<a href="AmendView.html.php">Amend/View Record</a>
+<a href="PersonReport.php">Report</a>
+
+
+</body>
+</html>
