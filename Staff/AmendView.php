c997309dc05b6d7c0fa541b336201a42ae70c0bc
c00300786 <c00300786@setu.ie>
2025-04-03 12:16:45 +0100

    amend fix

--- Staff/AmendView.php
+++ Staff/AmendView.php
@@ -10,7 +10,7 @@ $dbDate = date("Y-m-d", strtotime($_POST['amendDOB'])); //to match date format i
 
 $sql = "UPDATE Staff SET Firstname = '$_POST[amendfirstname]',
         Surname = '$_POST[amendlastname]',
-        DOB = '$dbDate',PhoneNum = '$_POST[amendphone]',JobTitle = '$_POST[amendjob],
+        DOB = '$dbDate',PhoneNum = '$_POST[amendphone]', JobTitle = '$_POST[amendjob]',
         DeletedFlag = $_POST[amendflag]' WHERE StaffId = '$_POST[amendid]' ";
 
 if (!mysqli_query($con, $sql))
